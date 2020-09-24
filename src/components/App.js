a69c6226fcfac0ed2887bbf2015053e605d9f433
A1DS19 <jp06171@gmail.com>
2020-09-23 20:23:30 -0600

    Rutas agregadas

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-// styles
-import '../assets/css/bootstrap.min.css';
-import '../assets/scss/paper-kit.scss?v=1.2.0';
-import '../assets/demo/demo.css?v=1.2.0';
-
 // components
 import Landing from './Landing';
-import Navbar from './Navbars/IndexNavbar';
+import Register from '../components/Auth/Register';
+import Login from '../components/Auth/Login';
+import NavbarIndex from './resources/IndexNavbar';
+import Footer from './resources/Footer';
 
 export class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Navbar />
+        <NavbarIndex />
         <Switch>
           <Route exact path='/' component={Landing} />
+          <Route exact path='/auth/register' component={Register} />
+          <Route exact path='/auth/login' component={Login} />
         </Switch>
+        <Footer />
       </BrowserRouter>
     );
   }
