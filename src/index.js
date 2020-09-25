1c992d0ea9a8f19445301a7f8ab6db836ef95edf
A1DS19 <jp06171@gmail.com>
2020-09-24 18:16:05 -0600

    redux agregado

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 //css imports
 import './assets/css/bootstrap.min.css';
@@ -7,5 +11,14 @@ import './assets/scss/paper-kit.scss?v=1.2.0';
 import './assets/demo/demo.css?v=1.2.0';
 
 import App from './components/App';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//config
+const store = createStore(reducers, {}, composeWithDevTools(applyMiddleware(reduxThunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
