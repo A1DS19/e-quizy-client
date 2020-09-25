2ae8f20c9be3a6f54c7109165a947784da44f250
A1DS19 <jp06171@gmail.com>
2020-09-25 00:00:53 -0600

    auth reducer agregado

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { reducer as reduxForm } from 'redux-form';
+import authReducer from './Auth';
 
 export default combineReducers({
   form: reduxForm,
+  auth: authReducer,
 });
