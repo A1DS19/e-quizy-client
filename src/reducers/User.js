ec06d86658bf29f0b2de5d16246efba354c33fc4
A1DS19 <jp06171@gmail.com>
2020-09-26 23:40:52 -0600

    new reducer User

---
+++ src/reducers/User.js
@@ -0,0 +1,15 @@
+import { FETCH_USER } from '../actions/types';
+
+const INITIAL_STATE = {
+  currentUser: '',
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case FETCH_USER:
+      return { ...state, currentUser: action.payload };
+
+    default:
+      return state;
+  }
+};
