b5b009b7efd05c9a75dd25643004cee90b36c382
A1DS19 <jp06171@gmail.com>
2020-09-26 23:40:00 -0600

    user reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as reduxForm } from 'redux-form';
 import authReducer from './Auth';
+import userReducer from './User';
 
 export default combineReducers({
   form: reduxForm,
   auth: authReducer,
+  user: userReducer,
 });
