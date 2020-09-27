7e41d904923d57605d52ac5e1ca7ecd930c2642b
A1DS19 <jp06171@gmail.com>
2020-09-26 23:38:41 -0600

    nuevas rutas

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // components
 import Landing from './Landing';
-import Register from '../components/Auth/Register';
-import Login from '../components/Auth/Login';
 import NavbarIndex from './resources/IndexNavbar';
 import Footer from './resources/Footer';
+//Auth
+import Register from '../components/Auth/Register';
+import Login from '../components/Auth/Login';
+//Profile
+import UserProfile from '../components/Profile/UserProfile';
+import UserProfileSettings from '../components/Profile/UserProfileSettings';
 
 export class App extends Component {
   render() {
@@ -17,6 +21,12 @@ export class App extends Component {
           <Route exact path='/' component={Landing} />
           <Route exact path='/auth/register' component={Register} />
           <Route exact path='/auth/login' component={Login} />
+          <Route exact path='/profile/user_profile' component={UserProfile} />
+          <Route
+            exact
+            path='/profile/user_profile_settings'
+            component={UserProfileSettings}
+          />
         </Switch>
         <Footer />
       </BrowserRouter>
