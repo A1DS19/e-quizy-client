0db5a6a33c13e500c5de44dc1fe06cc80ea2b065
A1DS19 <jp06171@gmail.com>
2020-09-30 23:45:31 -0600

    acciones para direcciones

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,11 @@
-import { AUTH_ERROR, AUTH_USER, FETCH_USER, UPDATE_PERSONAL_DATA } from './types';
+import {
+  AUTH_ERROR,
+  AUTH_USER,
+  FETCH_USER,
+  FETCH_ADDRESSES,
+  REMOVE_ADDRESSES,
+  ADD_ADDRESS,
+} from './types';
 import axios from 'axios';
 
 //AUTH ACTIONS INICIO:
@@ -46,6 +53,7 @@ export const fetchUser = () => {
       },
     });
     console.log(res.data);
+    res.data.dateOfBirth = JSON.stringify(res.data.dateOfBirth).slice(1, 11);
     dispatch({ type: FETCH_USER, payload: res.data });
   };
 };
@@ -79,6 +87,50 @@ export const updatePersonalData = (formValues, callback) => {
       });
       console.log(res);
       dispatch({ type: FETCH_USER, payload: res.data });
+      callback();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+//USER ACTIONS FIN
+
+//DIRECCIONES INICIO
+export const fetchAddresses = () => {
+  return async (dispatch) => {
+    try {
+      const res = await axios.get('/api/address', {
+        headers: {
+          Authorization: localStorage.getItem('token'),
+        },
+      });
+      console.log(res.data);
+      dispatch({ type: FETCH_ADDRESSES, payload: res.data });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export const insertAddress = (formValues, callback) => {
+  return async (dispatch) => {
+    try {
+      await axios.post('/api/address/create', formValues, {
+        headers: {
+          Authorization: localStorage.getItem('token'),
+        },
+      });
+      callback();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export const deleteAddress = (id, callback) => {
+  return async (dispatch) => {
+    try {
+      await axios.delete(`/api/address/${id}`);
       callback();
     } catch (error) {
       console.log(error);
