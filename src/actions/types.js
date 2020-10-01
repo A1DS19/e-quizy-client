6e4f2946a58277657a984147688bb14943a78cad
A1DS19 <jp06171@gmail.com>
2020-09-30 23:45:55 -0600

    nuevos types

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,6 @@
 export const AUTH_USER = 'AUTH_USER';
 export const AUTH_ERROR = 'AUTH_ERROR';
 export const FETCH_USER = 'FETCH_USER';
-export const UPDATE_PERSONAL_DATA = 'UPDATE_PERSONAL_DATA';
+export const FETCH_ADDRESSES = 'FETCH_ADDRESSES';
+export const REMOVE_ADDRESSES = 'REMOVE_ADDRESSES';
+export const ADD_ADDRESS = 'ADD_ADDRESS';
