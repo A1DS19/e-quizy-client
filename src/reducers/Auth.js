c561f5dad2a7820e43a699e76795b7334575ee9c
A1DS19 <jp06171@gmail.com>
2020-10-06 23:45:53 -0600

    cambio a reducer

--- src/reducers/Auth.js
+++ src/reducers/Auth.js
@@ -9,6 +9,7 @@ export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case AUTH_USER:
       return { ...state, authenticated: action.payload };
+
     case AUTH_ERROR:
       return { ...state, errorMessage: action.payload };
 
