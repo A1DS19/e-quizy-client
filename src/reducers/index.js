cf66b307c6e12d0fd9219b9e52b31f122aa36c97
A1DS19 <jp06171@gmail.com>
2020-11-02 19:57:47 -0600

    nuevo reducer evals

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { reducer as reduxForm } from 'redux-form';
 import authReducer from './Auth';
 import userReducer from './User';
+import evalsReducer from './Evals';
 
 export default combineReducers({
   form: reduxForm,
   auth: authReducer,
   user: userReducer,
+  evals: evalsReducer,
 });
